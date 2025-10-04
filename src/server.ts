4c1e8eec554bb6321478e0b3ee7f5d6ed875ffc0
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-04 19:33:10 +0600

    DotEnv Variable set & MongoDB connected using Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import { Server } from "http"
 import app from "./app";
+import { envVars } from "./app/config/env";
+import mongoose from "mongoose";
 
 
 let server: Server;
 
-const PORT = 5000;
-
 
 const startServer = async()=>{
     try {
-        server = app.listen(PORT, ()=>{
-            console.log(`Server is Listening at Port ${PORT}`);
+        console.log(envVars.NODE_ENV);
+        await mongoose.connect(envVars.DB_URL);
+        console.log('Connected to DB!!')
+
+        
+        server = app.listen(envVars.PORT, ()=>{
+            console.log(`Server is Listening at Port ${envVars.PORT}`);
         });
     } catch (error) {
         console.log(error)
