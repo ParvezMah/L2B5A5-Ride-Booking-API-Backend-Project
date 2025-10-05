3e953c9f414d66bd5f8fcc7aa44d32e08fc6993e
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-05 09:11:25 +0600

    Added globalErrorHandler & notFound middleware and added cookieParser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express"
 import router from "./app/routes";
 import cors from "cors";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.use("/api/v1", router);
@@ -15,6 +19,13 @@ app.get("/", (req:Request, res:Response)=>{
         message: "Welcome to Our Organized Ride Booking API"
     })
 })
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+
+// Route Not Found
+app.use(notFound)
 
 
 
--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { createUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 
 const router = Router();
@@ -13,6 +15,7 @@ router.post("/register",
 
 router.get(
   "/all-users",
+  // checkAuth(Role.ADMIN, Role.SUPER_ADMIN), //Add login with Admin or Superadmin | SeedSuperadmin
   UserControllers.getAllUsers
 );
 
