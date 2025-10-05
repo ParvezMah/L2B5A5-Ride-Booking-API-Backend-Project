12dd6cbeb6b24c3451a1f20cf51e70c186c9916f
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-05 08:55:25 +0600

    Added Custom property for req.user in Express Request parameter

---
+++ src/app/middlewares/checkAuth.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import httpStatus from "http-status-codes";
+import AppError from "../errorHelpers/appError";
+import { verifyToken } from "../utils/jwt";
+import { envVars } from "../config/env";
+import { User } from "../module/user/user.model";
+
+
+export const checkAuth =
+  (...authRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization || req.cookies.accessToken;
+
+      if (!accessToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No Token Recieved");
+      }
+      const verifiedToken = verifyToken(
+        accessToken,
+        envVars.JWT_ACCESS_SECRET
+      ) as JwtPayload;
+
+      const isUserExist = await User.findOne({ email: verifiedToken.email });
+
+      if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+      }
+      // if (isUserExist.isActive === false) {
+      //     throw new AppError(httpStatus.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+      // }
+      if (
+        authRoles.length &&
+        !authRoles
+          .map((r) => r.toLowerCase())
+          .includes(verifiedToken.role.toLowerCase())
+      ) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          "You are not promoted in this user"
+        );
+      }
+
+      req.user = verifiedToken;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
